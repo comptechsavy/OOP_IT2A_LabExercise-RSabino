7466c331f73e167540640b6eafecaa451e81365a
Reyjoy Peñanueva Sabino <reyjoy.sabino@dorsu.edu.ph>
2025-09-11 12:09:41 +0800

    Add files via upload

---
+++ Exer2_Sabino/CarTester.java
@@ -0,0 +1,28 @@
+public class CarTester {
+    public static void main(String[] args) {
+        
+        Car c1 = new Car("Toyota", "Hilux", 2025, "Black", "ABC123", "XYZ789", "250 km/h", "Gasoline");
+        Car c2 = new Car("Ford", "Ranger", 2025, "Red", "LCM123", "XYZ789", "250 km/h", "Gasoline");
+        Car c3 = new Car("Mitsubishi", "Montero", 2025, "White", "BDF123", "XYZ789", "250 km/h", "Gasoline");
+        Car c4 = new Car("Nissan", "Navarra", 2025, "Black", "XYZ123", "XYZ789", "250 km/h", "Gasoline");
+        Car c5 = new Car("Bently", "Ranger", 2025, "Black", "LMN123", "XYZ789", "250 km/h", "Gasoline");
+        Car c6 = new Car("Rolce Royce", "Ranger", 2025, "Black", "TBH123", "XYZ789", "250 km/h", "Gasoline");
+        Car c7 = new Car("Lamborgeni", "Ranger", 2025, "Red", "ADU123", "XYZ789", "250 km/h", "Gasoline");
+        Car c8 = new Car("Ford", "Mustang", 2025, "Black", "CFX123", "XYZ789", "250 km/h", "Gasoline");
+        Car c9 = new Car("Honda", "Cevic", 2025, "White", "BTC123", "XYZ789", "250 km/h", "Gasoline");
+        Car c10 = new Car("Audi", "Ranger", 2025, "Black", "ETH123", "XYZ789", "250 km/h", "Gasoline");        
+
+        c1.displayInfo();
+        c2.displayInfo();
+        c3.displayInfo();
+        c4.displayInfo();
+        c5.displayInfo();
+        c6.displayInfo();
+        c7.displayInfo();
+        c8.displayInfo();
+        c9.displayInfo();
+        c10.displayInfo();
+
+    }
+}
+
